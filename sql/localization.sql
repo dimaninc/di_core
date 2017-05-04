54741135a77ae9b4278e37179a73b3d3330ee9dc
Dmitry Petrov <dimaninc@gmail.com>
2017-05-04 17:40:09 +0300

    localization table update: value varchar->text

--- sql/localization.sql
+++ sql/localization.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `localization` (
   `id` bigint NOT NULL auto_increment,
   `name` varchar(255) NOT NULL,
-  `value` varchar(255) NOT NULL,
-  `en_value` varchar(255) NOT NULL,
+  `value` TEXT,
+  `en_value` TEXT,
   UNIQUE INDEX `name_idx` (`name`),
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
