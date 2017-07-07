e142e15ab4b0995970804886246b2d29126b0c2b
Dmitry Petrov <dimaninc@gmail.com>
2017-07-07 19:48:43 +0300

    create_work_folders.sh

---
+++ scripts/create_work_folders.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+mkdir -p _admin/_inc/cache
+chmod -R 777 _admin/_inc/cache
+
+mkdir -p _cfg/cache
+chmod -R 777 _cfg/cache
+
+chmod -R 777 db/dump/tables
+
+mkdir -p htdocs/uploads
+chmod -R 777 htdocs/uploads
+
+mkdir -p log/db
+chmod -R 777 log/db
+
+mkdir -p log/debug
+chmod -R 777 log/debug
