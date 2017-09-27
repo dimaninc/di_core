a6cbbc656e1de877317a1bcee3a086f8e07fd05a
Dmitry Petrov <dimaninc@gmail.com>
2017-09-27 19:45:55 +0300

    create_work_folders.sh db folder updates

--- scripts/create_work_folders.sh
+++ scripts/create_work_folders.sh
@@ -5,7 +5,8 @@ chmod -R 777 _admin/_inc/cache
 mkdir -p _cfg/cache
 chmod -R 777 _cfg/cache
 
-chmod -R 777 db/dump/tables
+mkdir -p db/dump
+chmod -R 777 db/dump
 
 mkdir -p htdocs/uploads
 chmod -R 777 htdocs/uploads
