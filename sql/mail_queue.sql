76e99f0a6dfe647b57b5f79002763c35ff4e557a
Dmitry Petrov <dimaninc@gmail.com>
2017-12-15 19:13:23 +0300

    mail_queue.sql updated: reply_to, settings added

--- sql/mail_queue.sql
+++ sql/mail_queue.sql
@@ -1,17 +1,22 @@
-CREATE TABLE IF NOT EXISTS mail_queue(
-  id bigint not null auto_increment,
-  date timestamp default CURRENT_TIMESTAMP,
-  sender varchar(255),
-  recipient varchar(255),
-  recipient_id bigint default '0',
-  subject varchar(255),
-  body mediumtext,
-  plain_body tinyint default '1',
-  attachment MEDIUMBLOB,
-  incut_ids varchar(20) default '',
-  visible tinyint default '1',
-  sent tinyint default '0',
-  news_id bigint default '0',
-  primary key(id),
-  key idx(visible,sent)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+CREATE TABLE IF NOT EXISTS mail_queue (
+  id           BIGINT NOT NULL AUTO_INCREMENT,
+  date         TIMESTAMP       DEFAULT CURRENT_TIMESTAMP,
+  sender       VARCHAR(255),
+  recipient    VARCHAR(255),
+  recipient_id BIGINT          DEFAULT '0',
+  reply_to     VARCHAR(255)    DEFAULT '',
+  subject      VARCHAR(255),
+  body         MEDIUMTEXT,
+  plain_body   TINYINT         DEFAULT '1',
+  attachment   MEDIUMBLOB,
+  incut_ids    VARCHAR(20)     DEFAULT '',
+  visible      TINYINT         DEFAULT '1',
+  sent         TINYINT         DEFAULT '0',
+  news_id      BIGINT          DEFAULT '0',
+  settings     TEXT,
+  PRIMARY KEY (id),
+  KEY idx(visible, sent)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_general_ci;
