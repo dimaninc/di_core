72ee53092eba64f77fe4e292764bc6556143eb0f
Dmitry Petrov <dimaninc@gmail.com>
2018-01-19 17:53:43 +0300

    ResetPassword module

---
+++ php/src/diCore/Module/ResetPassword.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dimaninc
+ * Date: 06.01.16
+ * Time: 10:40
+ */
+
+namespace diCore\Module;
+
+class ResetPassword extends \diModule
+{
+	public function render()
+	{
+		$this->getTwig()
+			->renderPage('reset_password/page');
+	}
+}
