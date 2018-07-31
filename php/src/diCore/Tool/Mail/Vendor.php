a906488a5510e69de49979916a4b337720934309
Dmitry Petrov <dimaninc@gmail.com>
2018-07-31 10:04:35 +0300

    Mail/Vendor.php: microsoft added

--- php/src/diCore/Tool/Mail/Vendor.php
+++ php/src/diCore/Tool/Mail/Vendor.php
@@ -8,30 +8,33 @@
 
 namespace diCore\Tool\Mail;
 
-
 use diCore\Tool\SimpleContainer;
 
 class Vendor extends SimpleContainer
 {
 	const google = 1;
 	const yandex = 2;
+	const microsoft = 3;
 	const own = 50;
 
 	public static $titles = [
 		self::google => 'Google',
 		self::yandex => 'Yandex',
+		self::microsoft => 'Microsoft',
 		self::own => 'Own SMTP',
 	];
 
 	public static $names = [
 		self::google => 'google',
 		self::yandex => 'yandex',
+		self::microsoft => 'microsoft',
 		self::own => 'own',
 	];
 
 	protected static $smtpHosts = [
 		self::google => 'smtp.gmail.com',
 		self::yandex => 'smtp.yandex.ru',
+		self::microsoft => 'smtp.office365.com',
 	];
 
 	protected static $smtpPorts = [
@@ -41,6 +44,10 @@ class Vendor extends SimpleContainer
 		],
 		self::yandex => [
 			true => 25,
+			false => 25,
+		],
+		self::microsoft => [
+			true => 587,
 			false => 25,
 		],
 	];
