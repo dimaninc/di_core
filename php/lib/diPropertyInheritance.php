619e6d5f20135436fedbdd6f096ec78093d0b365
Dmitry Petrov <dimaninc@gmail.com>
2018-08-11 10:45:04 +0300

    diPropertyInheritance: inherit param added

--- php/lib/diPropertyInheritance.php
+++ php/lib/diPropertyInheritance.php
@@ -24,20 +24,25 @@ class diPropertyInheritance
 		$this->ar[] = (array)(is_object($rec) && $rec instanceof \diModel ? $rec->get() : $rec);
 	}
 
-	public function get($field)
+	public function get($field, $inherit = true)
 	{
-		$rec = $this->getRec($field);
+		$rec = $this->getRec($field, $inherit);
 
 		return $rec ? $rec[$field] : null;
 	}
 
-	public function getRec($field)
+	public function getRec($field, $inherit = true)
 	{
 		for ($i = count($this->ar) - 1; $i >= 0; $i--)
 		{
 			if (!empty($this->ar[$i][$field]))
 			{
 				return $this->ar[$i];
+			}
+
+			if (!$inherit)
+			{
+				break;
 			}
 		}
 
