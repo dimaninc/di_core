d46fca9ac185f7b6b11c98c912663904d14ffce5
Dmitry Petrov <dimaninc@gmail.com>
2019-06-06 12:10:32 +0300

    video vendors: OK full support, VK and Facebook partial

--- php/lib/diVideoVendors.php
+++ php/lib/diVideoVendors.php
@@ -12,19 +12,47 @@ class diVideoVendors
 	const YouTube = 1;
 	const Vimeo = 2;
 	const RuTube = 3;
+	const Facebook = 4;
+	const Odnoklassniki = 5;
+	const Vk = 6;
 
 	public static $titles = [
 		self::Own => "Собственное видео",
 		self::YouTube => "YouTube",
 		self::Vimeo => "Vimeo",
         self::RuTube => 'RuTube',
+        //self::Facebook => 'Facebook', // todo: embed
+        self::Odnoklassniki => 'Odnoklassniki',
+        self::Vk => 'VK', // todo: embed https://toster.ru/q/414920 https://toster.ru/q/233109
 	];
 
-	public static $patterns = [
-		self::YouTube => '/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?\"\']*).*/',
+	protected static $patterns = [
+		self::YouTube => '/^.*((youtu\.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?\"\']*).*/',
 		self::Vimeo => '/(https?:\/\/)?(www\.)?(player\.)?vimeo\.com\/([a-z]*\/)*([0-9]{6,11})[?]?.*/',
-        self::RuTube => '#(https?://)rutube.ru/video/([^/]+)/#',
+        self::RuTube => '#(https?://)rutube\.ru/video/([^/]+)/#',
+        self::Facebook => '#facebook\.com/watch/\?v=(\d+)#',
+        self::Odnoklassniki => '#//ok\.ru/video(embed)?/(\d+)#',
+        self::Vk => '#//vk\.com/(video(\d+)_(\d+)|video_ext\.php\?oid=-?(\d+)&id=(\d+))#',
 	];
+
+	protected static $links = [
+        self::YouTube => 'https://www.youtube.com/watch?v=%s',
+        self::Vimeo => 'https://vimeo.com/%s',
+        self::RuTube => 'https://rutube.ru/video/%s/',
+        self::Facebook => 'https://www.facebook.com/watch/?v=%s',
+        self::Odnoklassniki => 'https://ok.ru/video/%s',
+        self::Vk => 'https://vk.com/video-%s',
+    ];
+
+    protected static $embedLinks = [
+        self::YouTube => 'https://www.youtube.com/embed/%s',
+        self::Vimeo => 'https://player.vimeo.com/video/%s',
+        self::RuTube => 'https://rutube.ru/play/embed/%s',
+        self::Facebook => 'https://www.facebook.com/watch/?v=%s',
+        self::Odnoklassniki => 'https://ok.ru/videoembed/%s?nochat=1',
+        //self::Vk => 'https://vk.com/video_ext.php?oid=%s&id=%s&hash=%s&hd=2',
+        self::Vk => 'https://vk.com/video_ext.php?oid=%s',
+    ];
 
 	public static function extractInfoFromEmbed($embed)
 	{
@@ -33,26 +61,48 @@ class diVideoVendors
 			'video_uid' => null,
 		];
 
-		foreach (self::$patterns as $vendor => $pattern)
-		{
-			preg_match($pattern, $embed, $r);
+        foreach (self::$patterns as $vendor => $pattern) {
+            preg_match($pattern, $embed, $r);
+            $idx = null;
 
-			if ($vendor == self::YouTube && isset($r[7]))
-			{
-				$ar['video_uid'] = $r[7];
-				$ar['vendor'] = $vendor;
-			}
-			elseif ($vendor == self::Vimeo && isset($r[5]))
-			{
-				$ar['video_uid'] = $r[5];
-				$ar['vendor'] = $vendor;
-			}
-            elseif ($vendor == self::RuTube && isset($r[2]))
-            {
-                $ar['video_uid'] = $r[2];
-                $ar['vendor'] = $vendor;
+            switch ($vendor) {
+                case self::YouTube:
+                    $idx = 7;
+                    break;
+
+                case self::Vimeo:
+                    $idx = 5;
+                    break;
+
+                case self::RuTube:
+                case self::Odnoklassniki:
+                    $idx = 2;
+                    break;
+
+                case self::Facebook:
+                    $idx = 1;
+                    break;
+
+                case self::Vk:
+                    if (!empty($r[2]) && !empty($r[3])) {
+                        // video link
+                        $ar['video_uid'] = $r[2] . '_' . $r[3];
+                        $ar['vendor'] = $vendor;
+                    } elseif (!empty($r[4]) && !empty($r[5])) {
+                        // video link
+                        $ar['video_uid'] = $r[4] . '_' . $r[5];
+                        $ar['vendor'] = $vendor;
+                    }
+                    break;
             }
-		}
+
+            if ($idx && !empty($r[$idx])) {
+                $ar['video_uid'] = $r[$idx];
+                $ar['vendor'] = $vendor;
+
+                break;
+            }
+        }
 
 		return $ar;
 	}
@@ -74,8 +124,7 @@ class diVideoVendors
 
 	public static function getThumbnail($vendor, $videoUid)
 	{
-		switch ($vendor)
-		{
+		switch ($vendor) {
 			case self::YouTube:
 				return sprintf("//img.youtube.com/vi/%s/default.jpg", $videoUid); //http:
 
@@ -95,8 +144,7 @@ class diVideoVendors
 
 	public static function getPoster($vendor, $videoUid)
 	{
-		switch ($vendor)
-		{
+		switch ($vendor) {
 			case self::YouTube:
 				return sprintf("//img.youtube.com/vi/%s/0.jpg", $videoUid); //http:
 
@@ -120,28 +168,25 @@ class diVideoVendors
 
 	public static function getTitle($vendor, $videoUid)
 	{
-		switch ($vendor)
-		{
-			case self::YouTube:
-				$rawData = self::getFile("http://youtube.com/get_video_info?video_id=" . $videoUid);
-				if ($rawData)
-				{
-					parse_str($rawData, $data);
+        switch ($vendor) {
+            case self::YouTube:
+                $rawData = self::getFile("http://youtube.com/get_video_info?video_id=" . $videoUid);
+                if ($rawData) {
+                    parse_str($rawData, $data);
 
-					//"view_count"
-					if (isset($data["title"]))
-					{
-						return $data["title"];
-					}
-				}
-				break;
+                    //"view_count"
+                    if (isset($data["title"])) {
+                        return $data["title"];
+                    }
+                }
+                break;
 
-			case self::Vimeo:
-				$info = self::getVimeoData($videoUid);
+            case self::Vimeo:
+                $info = self::getVimeoData($videoUid);
 
-				return isset($info[0]['title'])
-					? $info[0]['title']
-					: null;
+                return isset($info[0]['title'])
+                    ? $info[0]['title']
+                    : null;
 
             case self::RuTube:
                 $info = self::getRuTubeData($videoUid);
@@ -149,15 +194,14 @@ class diVideoVendors
                 return isset($info['title'])
                     ? $info['title']
                     : null;
-		}
+        }
 
-		return null;
-	}
+        return null;
+    }
 
 	public static function getDescription($vendor, $videoUid)
 	{
-		switch ($vendor)
-		{
+		switch ($vendor) {
 			case self::YouTube:
 				break;
 
@@ -181,35 +225,15 @@ class diVideoVendors
 
 	public static function getLink($vendor, $videoUid)
 	{
-		switch ($vendor)
-		{
-			case self::YouTube:
-				return sprintf('https://www.youtube.com/watch?v=%s', $videoUid);
-
-			case self::Vimeo:
-				return sprintf('https://vimeo.com/%s', $videoUid);
-
-            case self::RuTube:
-                return sprintf('https://rutube.ru/video/%s/', $videoUid);
-		}
-
-		return null;
+		return !empty(self::$links[$vendor])
+            ? sprintf(self::$links[$vendor], $videoUid)
+            : null;
 	}
 
 	public static function getEmbedLink($vendor, $videoUid)
 	{
-		switch ($vendor)
-		{
-			case self::YouTube:
-				return sprintf('https://www.youtube.com/embed/%s', $videoUid);
-
-			case self::Vimeo:
-				return sprintf('https://player.vimeo.com/video/%s', $videoUid);
-
-            case self::RuTube:
-                return sprintf('https://rutube.ru/play/embed/%s', $videoUid);
-		}
-
-		return null;
+        return !empty(self::$embedLinks[$vendor])
+            ? sprintf(self::$embedLinks[$vendor], $videoUid)
+            : null;
 	}
 }
