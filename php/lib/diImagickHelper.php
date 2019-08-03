54333c41623416bfed6268372d513370177c0f72
Dmitry Petrov <dimaninc@gmail.com>
2019-08-03 12:20:21 +0300

    ImagickHelper updates

--- php/lib/diImagickHelper.php
+++ php/lib/diImagickHelper.php
@@ -1,42 +1,40 @@
 <?php
-
 /**
  * Created by PhpStorm.
  * User: dimaninc
  * Date: 22.04.2016
  * Time: 16:57
  */
+
+use diCore\Tool\Logger;
+
 class diImagickHelper
 {
 	public static function getDraw($options = [])
 	{
 		$options = extend([
-			"font" => null,
-			"size" => null,
-			"color" => null,
-			"gravity" => null,
+			'font' => null,
+			'size' => null,
+			'color' => null,
+			'gravity' => null,
 		], $options);
 
 		$draw = new \ImagickDraw();
 
-		if ($options["font"] !== null)
-		{
-			$draw->setFont($options["font"]);
+		if ($options['font'] !== null) {
+			$draw->setFont($options['font']);
 		}
 
-		if ($options["size"] !== null)
-		{
-			$draw->setFontSize($options["size"]);
+		if ($options['size'] !== null) {
+			$draw->setFontSize($options['size']);
 		}
 
-		if ($options["color"] !== null)
-		{
-			$draw->setFillColor($options["color"]);
+		if ($options['color'] !== null) {
+			$draw->setFillColor($options['color']);
 		}
 
-		if ($options["gravity"] !== null)
-		{
-			$draw->setGravity($options["gravity"]);
+		if ($options['gravity'] !== null) {
+			$draw->setGravity($options['gravity']);
 		}
 
 		return $draw;
@@ -50,20 +48,20 @@ class diImagickHelper
 	public static function printText(\Imagick $im, $text, $options = [])
 	{
 		$options = extend([
-			"font" => null,
-			"size" => null,
-			"color" => null,
-			"x" => null,
-			"y" => null,
-			"angle" => 0,
+			'font' => null,
+			'size' => null,
+			'color' => null,
+			'x' => null,
+			'y' => null,
+			'angle' => 0,
 		], $options);
 
 		$draw = self::getDraw($options);
 		//$metrics = $im->queryFontMetrics($draw, $text);
-		//$delta = ($metrics["textHeight"] ?: $options["size"]);
-		$delta = $options["size"];
+		//$delta = ($metrics['textHeight'] ?: $options['size']);
+		$delta = $options['size'];
 
-		$im->annotateImage($draw, $options["x"], $options["y"] + $delta, $options["angle"], $text);
+		$im->annotateImage($draw, $options['x'], $options['y'] + $delta, $options['angle'], $text);
 	}
 
 	/**
@@ -77,73 +75,92 @@ class diImagickHelper
 	public static function printWrappedText(\Imagick $im, $text, $options = [])
 	{
 		$options = extend([
-			"return" => false, // if true, an image returns
-			"font" => null,
-			"size" => null,
-			"color" => null,
-			"w" => null,
-			"h" => null,
-			"x" => null,
-			"y" => null,
-			"fontLineHeight" => null,
-			"defaultLineHeight" => null,
-			"tuneCoordinatesCallback" => function($options, $realHeight) {
+			'return' => false, // if true, an image returns
+			'font' => null,
+			'size' => null,
+			'color' => null,
+			'w' => null,
+			'h' => null,
+			'x' => null,
+			'y' => null,
+			'fontLineHeight' => null,
+			'defaultLineHeight' => null,
+			'tuneCoordinatesCallback' => function($options, $realHeight) {
 				$deltaX = $deltaY = 0;
 
 				// Y-coordinate for center gravity
-				if ($options["gravity"] == "center" || $options["gravity"] == Imagick::GRAVITY_CENTER)
-				{
-					if ($realHeight < $options["h"])
-					{
-						$deltaY = ($options["h"] - $realHeight) >> 1;
+				if ($options['gravity'] == 'center' || $options['gravity'] == Imagick::GRAVITY_CENTER) {
+					if ($realHeight < $options['h']) {
+						$deltaY = ($options['h'] - $realHeight) >> 1;
 					}
 				}
 
 				return [$deltaX, $deltaY];
 			},
-			"gravity" => null,
+			'gravity' => null,
+            'extendImageIfNeeded' => false,
+            'debug' => false,
 		], $options);
 
 		$draw = new \ImagickDraw();
-		$draw->setFont($options["font"]);
-		$draw->setFontSize($options["size"]);
-		$draw->setFillColor($options["color"]);
-		//$draw->setTextUnderColor("#FFFF00");
+		$draw->setFont($options['font']);
+		$draw->setFontSize($options['size']);
+		$draw->setFillColor($options['color']);
+		//$draw->setTextUnderColor('#FFFF00');
 
-		$info = self::getWordWrapInfo($im, $draw, $text, $options["w"]);
-		$lines = $info["lines"];
-		$lineHeight = $info["lineHeight"];
+        $width = $options['w'];
+        $height = $options['h'];
 
-		simple_debug("{$options["font"]} lineHeight calculated = $lineHeight, gotten from settings = " . $options["fontLineHeight"]);
+		$info = self::getWordWrapInfo($im, $draw, $text, $width);
+		$lines = $info['lines'];
+		$lineHeight = $info['lineHeight'];
 
-		if ($options["fontLineHeight"] && (!$lineHeight || $lineHeight > $options["fontLineHeight"]))
-		{
-			$lineHeight = $options["fontLineHeight"];
+		if ($options['debug']) {
+            Logger::getInstance()->log(
+                "{$options['font']} lineHeight calculated = $lineHeight, gotten from settings = " . $options['fontLineHeight']
+            );
+            Logger::getInstance()->variable(
+                '$info', $info
+            );
+        }
+
+		if ($options['fontLineHeight'] && (!$lineHeight || $lineHeight > $options['fontLineHeight'])) {
+			$lineHeight = $options['fontLineHeight'];
 		}
 
-		$lineHeight = $lineHeight ?: $options["size"] * $options["fontLineHeight"];
+		$lineHeight = $lineHeight ?: $options['size'] * $options['fontLineHeight'];
 
-		simple_debug("final lineHeight = $lineHeight");
+        $options['debug'] && Logger::getInstance()->log("final lineHeight = $lineHeight");
+
+        if ($options['extendImageIfNeeded']) {
+            if ($info['width'] > $width) {
+                $width = $info['width'];
+            }
+
+            if ($lineHeight > $height) {
+                $height = $lineHeight * count($lines) * 2;
+            }
+        }
 
 		$label = new \Imagick();
-		$label->newImage($options["w"], $options["h"] ?: $lineHeight * 2, self::transparentColor()); //"none"
+		$label->newImage($width, $height ?: $lineHeight * count($lines) * 2, self::transparentColor());
 
-		list($deltaX, $deltaY) = $options["tuneCoordinatesCallback"]
-			? $options["tuneCoordinatesCallback"]($options, $lineHeight * count($lines))
+		list($deltaX, $deltaY) = $options['tuneCoordinatesCallback']
+			? $options['tuneCoordinatesCallback']($options, $lineHeight * count($lines))
 			: [0, 0];
 
-		for ($i = 0; $i < count($lines); $i++)
-		{
+		for ($i = 0; $i < count($lines); $i++) {
 			$deltaX = 0;
 
-			if ($options["gravity"] == "center" || $options["gravity"] == Imagick::GRAVITY_CENTER)
-			{
+			if (
+			    $options['gravity'] == 'center' ||
+                $options['gravity'] == Imagick::GRAVITY_CENTER
+            ) {
 				$metrics = $im->queryFontMetrics($draw, $lines[$i]);
 				$textWidth = $metrics['textWidth'];
 
-				if ($textWidth < $options["w"])
-				{
-					$deltaX = ($options["w"] - $textWidth) >> 1;
+				if ($textWidth < $width) {
+					$deltaX = ($width - $textWidth) >> 1;
 				}
 			}
 
@@ -151,20 +168,32 @@ class diImagickHelper
 			// ($i + 1) - adding an extra line-height, because imagick uses Y coordinate as a lower base line for text
 			$y = ($i + 1) * $lineHeight + $deltaY;
 
-			simple_debug("[$x, $y]: {$lines[$i]}");
+            $options['debug'] && Logger::getInstance()->log("[$x, $y]: {$lines[$i]}");
 
 			$label->annotateImage($draw, $x, $y, 0, $lines[$i]);
 		}
 
 		$draw->clear();
 
-		if ($options["return"])
-		{
+		if ($options['extendImageIfNeeded']) {
+		    // trim
+		    $label->trimImage(0);
+            $label->setImagePage(0, 0, 0, 0);
+
+            // extend back to needed size
+            $extWidth = max($options['w'], $label->getImageWidth());
+            $extHeight = max($options['h'], $label->getImageHeight());
+            $x = round(($extWidth - $label->getImageWidth()) / 2);
+            $y = round(($extHeight - $label->getImageHeight()) / 2);
+
+            $label->setImageBackgroundColor(self::transparentColor());
+            $label->extentImage($extWidth, $extHeight, -$x, -$y);
+        }
+
+		if ($options['return']) {
 			return $label;
-		}
-		else
-		{
-			$im->compositeImage($label, \Imagick::COMPOSITE_DEFAULT, $options["x"], $options["y"]);
+		} else {
+			$im->compositeImage($label, \Imagick::COMPOSITE_DEFAULT, $options['x'], $options['y']);
 			$label->clear();
 
 			return null;
@@ -180,12 +209,11 @@ class diImagickHelper
 	 */
 	public static function getWordWrapInfo(\Imagick $image, \ImagickDraw $draw, $text, $maxWidth = null)
 	{
-		if (!$draw->getFont())
-		{
-			throw new \Exception("Font not provided");
+		if (!$draw->getFont()) {
+			throw new \Exception('Font not provided');
 		}
 
-		$words = $maxWidth ? explode(" ", $text) : [$text];
+		$words = $maxWidth ? explode(' ', $text) : [$text];
 		$lines = [];
 		$i = 0;
 		$lineHeight = 0;
@@ -204,7 +232,7 @@ class diImagickHelper
 			}
 			*/
 
-			$metrics = $image->queryFontMetrics($draw, $currentLine . ($i < count($words) - 1 ? ' ' . $words[$i + 1] : ""));
+			$metrics = $image->queryFontMetrics($draw, $currentLine . ($i < count($words) - 1 ? ' ' . $words[$i + 1] : ''));
 
 			while ($metrics['textWidth'] <= $maxWidth && $i < count($words) - 1)
 			{
@@ -233,10 +261,10 @@ class diImagickHelper
 		}
 
 		return [
-			"lines" => $lines,
-			"lineHeight" => $lineHeight,
-			"width" => $realMaxWidth,
-			"height" => count($lines) * $lineHeight,
+			'lines' => $lines,
+			'lineHeight' => $lineHeight,
+			'width' => $realMaxWidth,
+			'height' => count($lines) * $lineHeight,
 		];
 	}
 
