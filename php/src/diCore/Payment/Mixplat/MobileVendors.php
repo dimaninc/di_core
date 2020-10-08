654bbdb62338e581bcb84be70e60832607621295
Dmitry Petrov <dimaninc@gmail.com>
2020-10-08 09:58:22 +0300

    mixplat: yota

--- php/src/diCore/Payment/Mixplat/MobileVendors.php
+++ php/src/diCore/Payment/Mixplat/MobileVendors.php
@@ -16,12 +16,14 @@ class MobileVendors extends VendorContainer
 	const MEGAFON = 2;
 	const MTS = 3;
 	const TELE2 = 4;
+    const YOTA = 5;
 
 	public static $names = [
 		self::BEELINE => 'beeline',
 		self::MEGAFON => 'mf',
 		self::MTS => 'mts',
 		self::TELE2 => 'tele2',
+        self::YOTA => 'yota',
 	];
 
 	public static $titles = [
@@ -29,5 +31,14 @@ class MobileVendors extends VendorContainer
 		self::MEGAFON => 'Мегафон',
 		self::MTS => 'МТС',
 		self::TELE2 => 'Теле2',
+        self::YOTA => 'Йота',
 	];
+
+    public static $codes = [
+        self::BEELINE => 'mobile_ru_beeline',
+        self::MEGAFON => 'mobile_ru_mf',
+        self::MTS => 'mobile_ru_mts',
+        self::TELE2 => 'mobile_ru_tele2',
+        self::YOTA => 'mobile_ru_yota',
+    ];
 }
