3fcf40dd69085dc4a08f8c4a1f0438df53e3954b
Dmitry Petrov <dimaninc@gmail.com>
2022-02-16 09:32:13 +0300

    cli controller: params

--- php/workers/cli.php
+++ php/workers/cli.php
@@ -4,6 +4,7 @@ require dirname(__FILE__) . '/../cliHelper.php';
 if (\diRequest::get('controller') && \diRequest::get('action')) {
 	\diBaseController::autoCreate(
 	    \diRequest::get('controller'),
-        \diRequest::get('action')
+        \diRequest::get('action'),
+        explode('/', \diRequest::get('params'))
     );
 }
