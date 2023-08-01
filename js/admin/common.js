ec2f86966bfbb12b050416d41ed4a170ddbe9dce
Dmitry Petrov <dimaninc@gmail.com>
2023-08-01 11:39:45 +0300

    ajaxErrorHandler added

--- js/admin/common.js
+++ js/admin/common.js
@@ -6,3 +6,30 @@ $(function () {
         dip.disableAutoPosition();
     }
 });
+
+// @todo use when rest api enabled
+function ajaxErrorHandler(err, fallbackMessage) {
+    function getMessage() {
+        if (typeof err === 'object' && err.message) {
+            return err.message;
+        }
+
+        const res = JSON.parse(err.responseText);
+
+        return (
+            res?.message ||
+            (typeof res === 'string' && res) ||
+            fallbackMessage ||
+            'Error during request'
+        );
+    }
+
+    const message = getMessage();
+
+    if (!A) {
+        console.error(message);
+        return;
+    }
+
+    A.console.add(message);
+}
