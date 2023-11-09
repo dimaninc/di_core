d7300a90e7c86ef85d868b29e377ea9223c0fcab
Dmitry Petrov <dimaninc@gmail.com>
2023-11-09 13:59:05 +0300

    ActiveInside traits pair for entities

---
+++ php/src/diCore/Traits/Collection/ActiveInside.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dimaninc
+ * Date: 09.11.2023
+ * Time: 10:53
+ */
+
+namespace diCore\Traits\Collection;
+
+/**
+ * Trait ActiveInside
+ * @package diCore\Traits\Collection
+ *
+ * @method $this filterByActive($value, $operator = null)
+ *
+ * @method $this orderByActive($direction = null)
+ *
+ * @method $this selectActive
+ */
+trait ActiveInside
+{
+}
---
+++ php/src/diCore/Traits/Model/ActiveInside.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dimaninc
+ * Date: 09.11.2023
+ * Time: 10:53
+ */
+
+namespace diCore\Traits\Model;
+
+/**
+ * Trait ActiveInside
+ * @package diCore\Traits\Model
+ *
+ * @method integer getActive
+ *
+ * @method bool hasActive
+ *
+ * @method $this setActive($value)
+ */
+trait ActiveInside
+{
+}
