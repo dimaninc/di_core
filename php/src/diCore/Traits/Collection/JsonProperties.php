c2b1d7edbbae8ab4ddc917ebf98ea6afebe11b36
Dmitry Petrov <dimaninc@gmail.com>
2025-04-17 16:50:54 +0300

    added Collection\JsonProperties trait

---
+++ php/src/diCore/Traits/Collection/JsonProperties.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace diCore\Traits\Collection;
+
+/**
+ * Trait JsonProperties
+ * @package diCore\Traits\Collection
+ *
+ * @method $this filterByProperties($value, $operator = null)
+ *
+ * @method $this orderByProperties($direction = null)
+ *
+ * @method $this selectProperties
+ */
+trait JsonProperties
+{
+}
