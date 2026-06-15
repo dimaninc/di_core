958296affa606525cbef45cf96d28dae7fdbc6bb
Dmitry Petrov <dimaninc@gmail.com>
2026-06-15 17:47:29 +0300

    updated dipopup types

--- ts/declarations/dipopup.d.ts
+++ ts/declarations/dipopup.d.ts
@@ -1,12 +1,22 @@
-declare var dip: {
+type TEventCallback = (props: {
+  name: string;
+  id: string;
+  element: JQuery;
+  diPopup: TDiPopups;
+}) => void;
+type TPopupCallback = (dip: TDiPopups, name: string) => void;
+
+type TDiPopups = {
   disableAutoPosition: VoidFunction;
-  setEvent: (id: string, eventName: string, callback: VoidFunction) => this;
+  setEvent: (id: string, eventName: string, callback: TEventCallback) => this;
   fireEvent: (id: string, eventName: string) => this;
-  setCallback: (name: string, callback: VoidFunction) => this;
-  getCallback: (name: string) => VoidFunction;
+  setCallback: (name: string, callback: TPopupCallback) => this;
+  getCallback: (name: string) => TPopupCallback;
   show: (name: string) => boolean;
   hide: (name: string) => boolean;
   hide_all: () => boolean;
   exists: (name: string) => boolean;
   visible: (name: string) => boolean;
 };
+
+declare var dip: TDiPopups;
